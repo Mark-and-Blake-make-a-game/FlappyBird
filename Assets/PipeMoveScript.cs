de55a37ef2d558bbb218d579e803d18e28b28b5d
Mark Bell <skrambelled@gmail.com>
2023-06-21 16:26:38 -0700

    delete pipes as they leave the game area

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
+    private float deadZone = -45;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,7 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (transform.position.x < deadZone)
+            Destroy(gameObject);
     }
 }
